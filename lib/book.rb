9f8a59276851c57598d9285618514496eeefd4d6
Lizzie Chan <lchan217@gmail.com>
2019-02-18 20:18:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book 
-  def initialize(title, author_name, page_count)
+  def initialize(title, author_name, page_count, genre)
     @title = title 
     @author_name = author_name
     @page_count = page_count
+    @genre = genre
   end 
 end
