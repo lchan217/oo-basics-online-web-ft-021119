195335e37bdc87c636384fc42b1b81e534519c75
Lizzie Chan <lchan217@gmail.com>
2019-02-18 20:20:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book 
   attr_accessor :title, :author_name, :page_count, :genre
-  def initialize(title, author_name, page_count, genre)
+  def initialize(title)
     @title = title 
     @author_name = author_name
     @page_count = page_count
