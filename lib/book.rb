7aebcd314b9423b8d23a41f9830c57d044eb9d30
Lizzie Chan <lchan217@gmail.com>
2019-02-18 20:24:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,7 +5,7 @@ class Book
     @title = title 
     @author = author
     @page_count = page_count
-   
+    @genre = genre
   end 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
